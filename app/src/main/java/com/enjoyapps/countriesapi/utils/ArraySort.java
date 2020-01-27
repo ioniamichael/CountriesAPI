1aa38f0a35e1d0f8cfce653ba2ef99fbbd5b1b02
ioniamichael <ioniamichael@gmail.com>
2020-01-27 11:47:00 +0200

    For push to master

--- app/src/main/java/com/enjoyapps/countriesapi/utils/ArraySort.java
+++ app/src/main/java/com/enjoyapps/countriesapi/utils/ArraySort.java
@@ -2,7 +2,6 @@ package com.enjoyapps.countriesapi.utils;
 
 import com.enjoyapps.countriesapi.pojo.Country;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
